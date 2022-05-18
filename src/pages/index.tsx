75fbe49bbc6820b70d01f1c75ecd3d160be72782
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-05-18 13:43:22 -0300

    changed entire project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,13 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import styled, { useTheme } from 'styled-components';
-
-const Xuize = styled.div`
-	background: red;
-	${props => props.theme.flex('center', 'center')}
-`;
 
 const Home: NextPage = () => {
-	const theme = useTheme();
 	return (
 		<>
 			<Head>
-				<title>Amazon - Home</title>
+				<title>Amazonas - Home</title>
 			</Head>
-			<>
-				<Xuize theme={theme}>
-					<h1>xuize</h1>
-				</Xuize>
-			</>
+			<></>
 		</>
 	);
 };
--- styled.d.ts
+++ styled.d.ts
@@ -1,9 +1,7 @@
 import 'styled-components';
 
-declare module 'styled-components' {
-	export interface DefaultTheme {
-		flex: (justify: string, align: string) => string;
-		flexcol: () => string;
-		colors?: object;
-	}
+export interface DefaultTheme {
+	flex: (justify: string, align: string) => string;
+	flexcol: () => string;
+	colors?: object;
 }
