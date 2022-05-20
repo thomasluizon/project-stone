f5c4c820b32080f4d8c0cf03a992fc7a313ae804
flaviozorzetto <flaviozorzetto11@gmail.com>
2022-05-20 10:44:42 -0300

    created homepage base

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,62 @@
 import type { NextPage } from 'next';
+import Container from '../global/Container';
+import { IProduct } from '../core/product';
+import styled, { useTheme } from 'styled-components';
+import Title from '../components/Title';
+
+const HomeSection = styled.section`
+   .wrapper {
+      ${props => props.theme.flex()}
+      &-col {
+         ${props => props.theme.flexcol()}
+      }
+   }
+
+   .trending-container {
+      .trending-card {
+         width: 50%;
+      }
+   }
+`;
+
+export const getStaticProps = async () => {
+   const stones = await fetch('https://project-stone.vercel.app/api/products')
+      .then(res => res.json())
+      .then(json => json);
+
+   return {
+      revalidate: 86400,
+      props: {
+         stones: stones.stones,
+      },
+   };
+};
 
 const Home: NextPage = (props: any) => {
-	return (
-		<>
-			<h1>oi</h1>
-		</>
-	);
+   const theme = useTheme();
+   const filtered = props.stones.sort((a: IProduct, b: IProduct) => {
+      return b.sales - a.sales;
+   });
+
+   const trending = [filtered[0], filtered[1], filtered[2]];
+   console.log(trending);
+
+   return (
+      <HomeSection theme={theme}>
+         <Container>
+            <div className="wrapper wrapper-col">
+               <Title>Most Selled</Title>
+               <div className="wrapper trending-container">
+                  {trending.map(stone => (
+                     <div className="trending-card">
+                        <img src={stone.image} />
+                     </div>
+                  ))}
+               </div>
+            </div>
+         </Container>
+      </HomeSection>
+   );
 };
 
 export default Home;
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -8,54 +8,54 @@ import { useEffect, useState } from 'react';
 import { IProduct } from '../core/product';
 
 export const getStaticProps = async () => {
-	const stones = await fetch('https://project-stone.vercel.app/api/products')
-		.then(res => res.json())
-		.then(json => json);
+   const stones = await fetch('https://project-stone.vercel.app/api/products')
+      .then(res => res.json())
+      .then(json => json);
 
-	return {
-		revalidate: 86400,
-		props: {
-			stones,
-		},
-	};
+   return {
+      revalidate: 86400,
+      props: {
+         stones,
+      },
+   };
 };
 
-const HomeSection = styled.section`
-	.wrapper {
-		${props => props.theme.flex()};
-		flex-direction: column;
-		gap: 2rem;
-	}
+const ProductsSection = styled.section`
+   .wrapper {
+      ${props => props.theme.flex()};
+      flex-direction: column;
+      gap: 2rem;
+   }
 `;
 
 const Products: NextPage = (props: any) => {
-	const [products, setProducts] = useState<IProduct[]>([]);
+   const [products, setProducts] = useState<IProduct[]>([]);
 
-	useEffect(() => {
-		const productsArr = props.stones.stones;
-		setProducts(productsArr);
-	}, [props.stones.stones]);
+   useEffect(() => {
+      const productsArr = props.stones.stones;
+      setProducts(productsArr);
+   }, [props.stones.stones]);
 
-	const theme = useTheme();
-	return (
-		<>
-			<Head>
-				<title>Project Stone - Home</title>
-			</Head>
-			<>
-				<HomeSection theme={theme}>
-					<Container>
-						<div className="wrapper">
-							<header>
-								<Title>Products</Title>
-							</header>
-							<ProductsComp products={products} />
-						</div>
-					</Container>
-				</HomeSection>
-			</>
-		</>
-	);
+   const theme = useTheme();
+   return (
+      <>
+         <Head>
+            <title>Project Stone - Home</title>
+         </Head>
+         <>
+            <ProductsSection theme={theme}>
+               <Container>
+                  <div className="wrapper">
+                     <header>
+                        <Title>Products</Title>
+                     </header>
+                     <ProductsComp products={products} />
+                  </div>
+               </Container>
+            </ProductsSection>
+         </>
+      </>
+   );
 };
 
 export default Products;
