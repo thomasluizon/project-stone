27176dfa15ff2dd4a0b3a36c5a38dbaa99019387
Thomas Luizon <thomaslrgregorio@gmail.com>
2022-05-26 17:07:26 -0300

    back: added categories to api

--- back-end/index.ts
+++ back-end/index.ts
@@ -4,57 +4,61 @@ const app = express();
 const prisma = new PrismaClient();
 
 async function getAllStones() {
-   const allStones = await prisma.stone.findMany();
+	const stones = await prisma.stone.findMany();
 
-   return allStones;
+	return stones;
 }
 
 async function getStone(id: number) {
-   const stone = await prisma.stone.findUnique({
-      where: {
-         id,
-      },
-   });
+	const stone = await prisma.stone.findUnique({
+		where: {
+			id,
+		},
+	});
 
-   return stone;
+	return stone;
 }
 
 app.get('/stones', async (req, res) => {
-   const allStones = await getAllStones();
-
-   res.json({
-      allStones,
-   });
+	const stones = await getAllStones();
+	const categories: string[] = [];
+	stones.forEach(stone => {
+		if (!categories.includes(stone.category)) categories.push(stone.category);
+	});
+	res.json({
+		stones,
+		categories,
+	});
 });
 
 app.get('/stone/:id', async (req, res) => {
-   const stone = await getStone(+req.params.id);
+	const stone = await getStone(+req.params.id);
 
-   res.json({
-      stone,
-   });
+	res.json({
+		stone,
+	});
 });
 
 const addStone = async (
-   name: string,
-   category: string,
-   description: string,
-   image: string,
-   price: number,
-   sales?: number,
-   id?: number
+	name: string,
+	category: string,
+	description: string,
+	image: string,
+	price: number,
+	sales?: number,
+	id?: number
 ) => {
-   await prisma.stone.create({
-      data: {
-         name,
-         category,
-         description,
-         image,
-         price,
-         sales: sales || undefined,
-         id: id || undefined,
-      },
-   });
+	await prisma.stone.create({
+		data: {
+			name,
+			category,
+			description,
+			image,
+			price,
+			sales: sales || undefined,
+			id: id || undefined,
+		},
+	});
 };
 
 app.listen(process.env.PORT || 3001, () => console.log('server running...\n'));
