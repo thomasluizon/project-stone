729c75c094279eee7c06d5c49b37fae497bb107c
flaviozorzetto <flaviozorzetto11@gmail.com>
2022-05-26 12:49:29 -0300

    back: created base routes

--- back-end/index.ts
+++ back-end/index.ts
@@ -3,45 +3,58 @@ import express from 'express';
 const app = express();
 const prisma = new PrismaClient();
 
-const main = async () => {
-	const allStones = await prisma.stone.findMany();
+async function getAllStones() {
+   const allStones = await prisma.stone.findMany();
 
-	app.listen(process.env.PORT || 3001, () =>
-		console.log('server running...\n')
-	);
-	app.get('/api', (req, res) => {
-		res.json({
-			allStones,
-		});
-	});
-};
+   return allStones;
+}
+
+async function getStone(id: number) {
+   const stone = await prisma.stone.findUnique({
+      where: {
+         id,
+      },
+   });
+
+   return stone;
+}
+
+app.get('/stones', async (req, res) => {
+   const allStones = await getAllStones();
+
+   res.json({
+      allStones,
+   });
+});
+
+app.get('/stone/:id', async (req, res) => {
+   const stone = await getStone(+req.params.id);
+
+   res.json({
+      stone,
+   });
+});
 
 const addStone = async (
-	name: string,
-	category: string,
-	description: string,
-	image: string,
-	price: number,
-	sales?: number,
-	id?: number
+   name: string,
+   category: string,
+   description: string,
+   image: string,
+   price: number,
+   sales?: number,
+   id?: number
 ) => {
-	await prisma.stone.create({
-		data: {
-			name,
-			category,
-			description,
-			image,
-			price,
-			sales: sales || undefined,
-			id: id || undefined,
-		},
-	});
+   await prisma.stone.create({
+      data: {
+         name,
+         category,
+         description,
+         image,
+         price,
+         sales: sales || undefined,
+         id: id || undefined,
+      },
+   });
 };
 
-main()
-	.catch(er => {
-		throw er;
-	})
-	.finally(async () => {
-		await prisma.$disconnect();
-	});
+app.listen(process.env.PORT || 3001, () => console.log('server running...\n'));
