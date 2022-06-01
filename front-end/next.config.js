31d1a3bc46f8bbddf94d702b0504986e0d7df145
flaviozorzetto <flaviozorzetto11@gmail.com>
2022-06-01 15:02:08 -0300

    front: fixed font

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,25 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	trailingSlash: true,
-	compiler: {
-		styledComponents: true,
-	},
-	images: {
-		domains: [
-			'st.depositphotos.com',
-			'st2.depositphotos.com',
-			'st3.depositphotos.com',
-			'st4.depositphotos.com',
-			'static3.depositphotos.com',
-			'static7.depositphotos.com',
-			'static5.depositphotos.com',
-			'static6.depositphotos.com',
-			'static8.depositphotos.com',
-			'static4.depositphotos.com',
-			'static9.depositphotos.com',
-		],
-	},
+   reactStrictMode: true,
+   trailingSlash: true,
+   compiler: {
+      styledComponents: true,
+   },
+   optimizeFonts: false,
+   images: {
+      domains: [
+         'st.depositphotos.com',
+         'st2.depositphotos.com',
+         'st3.depositphotos.com',
+         'st4.depositphotos.com',
+         'static3.depositphotos.com',
+         'static7.depositphotos.com',
+         'static5.depositphotos.com',
+         'static6.depositphotos.com',
+         'static8.depositphotos.com',
+         'static4.depositphotos.com',
+         'static9.depositphotos.com',
+      ],
+   },
 };
 
 module.exports = nextConfig;
